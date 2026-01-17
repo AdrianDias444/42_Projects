ab544ef6adbf7ac4f104c8bf6e4abe84861c36c4
adrian <adriandias0709@gmail.com>
2026-01-17 16:58:15 +0000

    push_swap quick sort upload

--- push_swap/quick_sort/ft_make_a_cicle.c
+++ push_swap/quick_sort/ft_make_a_cicle.c
@@ -4,20 +4,22 @@
 void ft_make_a_cicle(t_stack *stack_A, t_stack *stack_B)
 {
     t_node *pivot;
-    int count;
 
     pivot = ft_return_mid(stack_A);
-    //printf("/nPivot number is %d\n", pivot->nb);
     while(ft_min_node(stack_A)->nb <= pivot->nb)
     {
-        count = 1;
         while(stack_B->top && stack_B->top->next && ft_max_node(stack_B) != stack_B->top)
-            rrb(stack_B);
+        {
+            if(ft_calc_cost(stack_B, ft_max_node(stack_B)) == 1)
+                rb(stack_B);
+            else
+                rrb(stack_B);
+        }
         if(stack_A->top->nb <= pivot->nb)
         {
             if(!stack_B->top)
                 pb(stack_A, stack_B);
-            else if(!stack_B->top->next && count == 1)
+            else if(!stack_B->top->next)
             {
                 pb(stack_A, stack_B);
                 if(stack_B->top->nb < stack_B->top->next->nb)
@@ -35,5 +37,10 @@ void ft_make_a_cicle(t_stack *stack_A, t_stack *stack_B)
 
     }
     while(stack_B->top != ft_max_node(stack_B))
-        rrb(stack_B);
+    {
+        if(ft_calc_cost(stack_B, ft_max_node(stack_B)) == 1)
+            rb(stack_B);
+        else
+            rrb(stack_B);
+    }
 }
