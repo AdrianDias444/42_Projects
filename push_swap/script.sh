8443ba1d8c5896fc8b0ea1c8b7d385e15e4ac425
adrian <adriandias0709@gmail.com>
2026-02-15 09:42:24 +0000

    push swap finally done

--- push_swap/script.sh
+++ push_swap/script.sh
@@ -1,18 +1,19 @@
-#!/bin/bash
-
 soma=0
 
 for ((i = 0; i < 10; i++)); do
     ARG=($(shuf -i 1-2500 -n 500))
 
     output=$(./a.out "${ARG[@]}")
-
+    
     echo "$output" | ./checker_linux "${ARG[@]}"
-
+    
     nb=$(echo "$output" | wc -l)
     soma=$((soma + nb))
+    
+    valgrind --leak-check=full ./a.out "${ARG[@]}" 2>&1 | grep "total heap usage"
 
     printf "Iteração %d: %d linhas\n" "$i" "$nb"
+    echo "-----------------------------------"
 done
 
 media=$((soma / 10))
