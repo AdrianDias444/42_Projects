7be95086f112c4ae6af7adc11a4b0fb9b49cbea1
Adrian <adriandias0709@gmail.com>
2026-06-08 22:14:03 +0100

    M03 done

--- py_m/M03/ex5/ft_data_stream.py
+++ py_m/M03/ex5/ft_data_stream.py
@@ -1,50 +1,43 @@
-from typing import Generator
+#!/usr/bin/python3
+
+import random
+import typing
+
+def gen_event():
+    player = ["alice", "bob", "charlie", "dylan"]
+    actions = ["run", "eat", "sleep", "grab", "move", "climb", "swim", "release", "use"]
+    
+    while True:
+        name = random.choice(player)
+        action = random.choice(actions)   
+        yield(name, action)
 
 
-class StreamWizard():
-    def __init__(self):
-        pass
-    def generate_events(self, i:int) -> Generator:
-        players = ["adrian", "Odair", "George", ]
-        events = ["killed monster", "found treasure", "leveled up"]
+def consume_event(event_list):
+    while event_list:
+        event = random.choice(event_list)
+        event_list.remove(event)
+        yield event    
 
-        yield(
-            "alice",
-            "killed monster"
-        )
 
-        yield(
-            "bob",
-            "found treasure"
-        )
-        yield(
-            "charlie",
-            "levelep up"
-        )
-
-    def print_first_3_events(events):
-
-        for event in events:
-            print(f"Event [] {event[0]} {event[1]}")
-if __name__ == "__main__":
+def main():
     print("=== Game Data Stream Processor ===")
+    gerador = gen_event()
+    for i in range(1000):
+        evento = next(gerador)
+        print(f"Event {i}: Player {evento[0]} did action {evento[1]}")
     
-    manager = StreamWizard()
-    events = manager.generate_events(10)
+    gerador2 = gen_event()
+    lista = []
     
-    number_of_events = 1000
+    for i in range(10):
+        lista.append(next(gerador2))
+    
+    print(f"Built list of 10 events: {lista}")
+    
+    for event in consume_event(lista):
+        print(f"Got event from list: {event}")
+        print(f"Remains in list: {lista}")
 
-    for event in events:
-        print(f"{event[0]}, {event[1]}")
-
-""" 
-def fun(max):
-    i = 1
-    while i <= max:
-        if(i != 10):
-            yield i
-
-        i += 1
-count = fun(15)
-for i in count:
-    print(i) """
+if __name__ == "__main__":
+    main()
---
+++ py_m/M03/ex6/ft_data_alchemist.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import random
+
+def main():
+    print("=== Game Data Alchemist ===")
+    players = ['Alice', 'bob', 'Charlie', 'dylan', 'Emma', 'Gregory', 'john', 'kevin', 'Liam']
+    print(f"Initial list of players: {players}")
+    
+    all_cap = [nome.capitalize() for nome in players]
+    print(f"New list with all names capitalized: {all_cap}")
+    
+    only_cap = [nome for nome in players if nome[0].isupper()]
+    print(f"New list of capitalized names only: {only_cap}")
+    
+    scores = {nome: random.randint(0, 1000) for nome in all_cap}
+    print(f"Score dict: {scores}")
+    
+    avg = sum([scores[nome] for nome in scores]) / len(scores)
+    print(f"Score average is {round(avg, 2)}")
+    
+    high = {nome: scores[nome] for nome in scores if scores[nome] > avg}
+    print(f"High scores: {high}")
+
+if __name__ == "__main__":
+    main()
