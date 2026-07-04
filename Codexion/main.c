ab6a2a7d6612130087bfff4af14891e6c95d96c8
Adrian Dias <adriandias0709@gmail.com>
2026-07-04 23:19:12 +0100

    Codexion init

---
+++ Codexion/main.c
@@ -0,0 +1,18 @@
+#include "header.h"
+
+
+void* foo(void* arg) {
+    printf("Created a new thread");
+    return NULL;
+ }
+int main() {
+    
+    // Create a pthread_t variable to store
+    // thread ID
+    pthread_t thread1;
+    
+    // Creating a new thread. 
+    pthread_create(&thread1, NULL, foo, NULL);
+    pthread_join(thread1, NULL);
+    return 0;
+}
