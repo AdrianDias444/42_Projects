87c6c8e199e421fc0e332b0d63b7b6f93deb0267
Adrian Dias <adriandias0709@gmail.com>
2026-07-07 15:19:23 +0100

    CODEXION update

--- Codexion/main.c
+++ Codexion/main.c
@@ -14,7 +14,14 @@ int main(int argc, char **argv)
     // pthread_join(thread1, NULL);
 
     args = ft_parser(argc, argv);
-    printf("%d", args.number_of_coders);
-
+    printf("\n");
+    printf("Number of coders: %d\n", args.number_of_coders);
+    printf("Time to burnout: %d\n", args.time_to_burnout);
+    printf("Time to compile: %d\n", args.time_to_compile);
+    printf("Time to debug: %d\n", args.time_to_debug);
+    printf("Time to refactor: %d\n", args.time_to_refactor);
+    printf("Number of compiles: %d\n", args.number_of_compiles_required);
+    printf("Dongle Cooldown: %d\n", args.dongle_cooldown);
+    printf("Scheduler: %s\n", args.scheduler);
     return 0;
 }
